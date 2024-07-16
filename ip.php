66d56b1bcc8cc0fb47eac57555da31ed2728a3af
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-16 11:07:27 +0200

    Update ip.php
    split IPv4 & IPv6

--- ip.php
+++ ip.php
@@ -1,46 +1,45 @@
 <?php
-// Function to get the client's IP address
-function getClientIP() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if (isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
+// Function to get the client's IP addresses and separate them into IPv4 and IPv6
+function getClientIPs() {
+    $ips = ['IPv4' => [], 'IPv6' => []];
+    
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) addIP($_SERVER['HTTP_CLIENT_IP'], $ips);
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) addIP($_SERVER['HTTP_X_FORWARDED_FOR'], $ips);
+    if (isset($_SERVER['HTTP_X_FORWARDED'])) addIP($_SERVER['HTTP_X_FORWARDED'], $ips);
+    if (isset($_SERVER['HTTP_FORWARDED_FOR'])) addIP($_SERVER['HTTP_FORWARDED_FOR'], $ips);
+    if (isset($_SERVER['HTTP_FORWARDED'])) addIP($_SERVER['HTTP_FORWARDED'], $ips);
+    if (isset($_SERVER['REMOTE_ADDR'])) addIP($_SERVER['REMOTE_ADDR'], $ips);
+    
+    return $ips;
 }
 
-// Function to get all possible client IPs
-function getAllClientIPs() {
-    $ips = [];
-    if (isset($_SERVER['HTTP_CLIENT_IP'])) $ips[] = $_SERVER['HTTP_CLIENT_IP'];
-    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $ips[] = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    if (isset($_SERVER['HTTP_X_FORWARDED'])) $ips[] = $_SERVER['HTTP_X_FORWARDED'];
-    if (isset($_SERVER['HTTP_FORWARDED_FOR'])) $ips[] = $_SERVER['HTTP_FORWARDED_FOR'];
-    if (isset($_SERVER['HTTP_FORWARDED'])) $ips[] = $_SERVER['HTTP_FORWARDED'];
-    if (isset($_SERVER['REMOTE_ADDR'])) $ips[] = $_SERVER['REMOTE_ADDR'];
-    return $ips;
+// Helper function to add IP to the appropriate category
+function addIP($ip, &$ips) {
+    $ip = trim($ip);
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+        if (!in_array($ip, $ips['IPv4'])) $ips['IPv4'][] = $ip;
+    } elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+        if (!in_array($ip, $ips['IPv6'])) $ips['IPv6'][] = $ip;
+    }
+}
+
+// Function to get the first valid IP address (IPv4 or IPv6)
+function getFirstIP($ips) {
+    return !empty($ips['IPv4']) ? $ips['IPv4'][0] : (!empty($ips['IPv6']) ? $ips['IPv6'][0] : 'UNKNOWN');
 }
 
 // Determine the output mode
 $mode = isset($_GET['mode']) ? $_GET['mode'] : 'plaintext';
 
+// Get client IPs
+$ips = getClientIPs();
+
 // Output based on mode
 if ($mode == 'json') {
     header('Content-Type: application/json');
-    $ips = getAllClientIPs();
-    echo json_encode(['ip' => getClientIP(), 'all_ips' => $ips]);
+    echo json_encode(['ip' => getFirstIP($ips), 'all_ips' => $ips]);
 } else {
     header('Content-Type: text/plain');
-    echo getClientIP();
+    echo getFirstIP($ips);
 }
 ?>
